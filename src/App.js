827c1c65dcc0ac711a5d24f1a64b7f482b1b9ec1
mr_varshney2004 <130595295+varshneyboy2004@users.noreply.github.com>
2024-01-31 16:33:33 +0530

    change images and code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Lottie from 'lottie-react';
 import loadingAnimation from './loading.json';
 
-
-
- function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('');
+function App() {
+  const [data, setData] = useState({});
+  const [loc, setLoc] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=imperial&appid=b8beb8bcdd794dff7a9e3ad0ad1d232f`;
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
-  /* const [res,setRes] = useState("") */
 
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=2754dfca540b9ca12f00bcb0043febc9`;
-
-
-
-  /*   const options = {
-      method: 'GET',
-      url: 'https://forecast9.p.rapidapi.com/rapidapi/forecast/Ghaziabad/summary/',
-      headers: {
-        'X-RapidAPI-Key': 'f5f315d086msh22e4e2a7f438e71p16a8f8jsn1609b2aabfec',
-        'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
-      }
-    };
-    
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-      setRes(response)
-    } catch (error) {
-      console.error(error);
-    }
-     */
-
-
-
-
+  
   const dateBuilder = (d) => {
     let months = [
       "January",
@@ -74,30 +48,6 @@ import loadingAnimation from './loading.json';
     const seconds = d.getSeconds();
     return `${day} ${date} ${month} ${year} ${hours}:${minutes}:${seconds}`;
   }
-
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=2754dfca540b9ca12f00bcb0043febc9`
-        ).then((response) => {
-          setData(response.data);
-          setError(false);
-        }).catch((error) => {
-          setError(true);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }, (error) => {
-      setError(true);
-      setLoading(false);
-    }
-    );
-  }, []);
-
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentDateTime(new Date());
@@ -108,105 +58,136 @@ import loadingAnimation from './loading.json';
     };
   }, []);
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=b8beb8bcdd794dff7a9e3ad0ad1d232f`
+          )
+          .then((response) => {
+            setData(response.data);
+            setError(false);
+          })
+          .catch((error) => {
+            setError(true);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      },
+      (error) => {
+        setError(true);
+        setLoading(false);
+      }
+    );
+  }, []);
+
   const searchLocation = () => {
     setLoading(true);
-    axios.get(url).then((response) => {
-      setData(response.data)
-      setError(false);
-      setLocation("");
-      setTimeout(() => {
-        setLoading(false);
-      }, 1000);
-    })
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        setError(false);
+        setLoc("");
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      })
       .catch((error) => {
         setError(true);
         setLoading(false);
       });
+  };
 
-
-  }
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       searchLocation();
     }
   };
+
   return (
-    <div className='app'>
-      <div className='search'>
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={handleKeyPress}
-          placeholder='Enter Location'
-          type="text"
-        />
-        <button className="search-button" onClick={searchLocation}>
-          Search
-        </button>
+    <div className="app">
+      <div className="search-box">
+        <div className="search-input-container">
+          <input
+            value={loc}
+            onChange={(event) => setLoc(event.target.value)}
+            onKeyPress={handleKeyPress}
+            placeholder="Enter Your Location"
+            type="text"
+          />
+          <button className="search-button" onClick={searchLocation}>
+            Search
+          </button>
+        </div>
       </div>
-      <div className='container'>
+      <div className="container">
         {error ? (
           <div className="error">
             <h1>Wrong location entered</h1>
           </div>
         ) : (
-          <div className='top'>
-            <div className='location'>
-              <p>{data.name}</p>
+          <div className="top">
+            <div className="location">
+              <p className="loactiontext" >{data.name}</p>
             </div>
             <div className="weather-icon">
               {data.weather && data.weather[0] ? (
                 <img
                   src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                  alt={data.weather[0].description}
                 />
               ) : null}
             </div>
-            <div className='temp'>
-              {data.main ? <h1>{((data.main.temp - 32) * 5 / 9).toFixed()}°C</h1> : null}
-
+            <div className="temp">
+              {data.main ? <h1>{((data.main.temp - 32) * 5/9).toFixed()}°C</h1> : null}
             </div>
-            <div className='description'>
-              {data.weather ? <p>{data.weather[0].main}</p> : null}
-
+            <div className="description">
+              {data.weather ? (
+                <p className="bold">{data.weather[0].main}</p>
+              ) : null}
             </div>
           </div>
         )}
-
-        <div className='date-time'>
-          <p>{dateBuilder(currentDateTime)}</p>
+          <div className='container_date'>
+          <div className='date-time'>
+            <p>{dateBuilder(currentDateTime)}</p>
+          </div>
         </div>
-
-        {data.name != undefined && !error && (
-          <div className='bottom'>
-            <div className='feels'>
-              {data.main ? <p className='bold'>{((data.main.feels_like - 32) * 5 / 9).toFixed()}°C</p> : null}
-              <p>Feels like</p>
+        {data.name !==undefined && !error && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{((data.main.feels_like-32)*5/9).toFixed(2)}°C</p>
+              ) : null}
+              <p>Feels Like</p>
             </div>
-            <div className='humidity'>
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <div className="humidty">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
-            <div className='wind'>
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}MPH</p> : null}
+            <div className="wind">
+              {data.wind ? <p className="bold">{data.wind.speed}MPH</p> : null}
               <p>Wind Speed</p>
             </div>
           </div>
         )}
-        <div>
-
-        </div>
-
+         
         {loading && (
           <div className="loading-animation">
-            <Lottie loop={true} animationData={loadingAnimation} />
-            <h1 className="loadingtext">Loading...</h1>
-            <p className="loadingtext">Just Wait</p>
+          <Lottie loop={true} animationData={loadingAnimation} />
+          <h1 className="loadingtext">Loading...</h1>
+          <p className="loadingtext">Just Wait</p>
           </div>
         )}
-
-        {/* {res} */}
       </div>
     </div>
-  )
+  );
 }
+
 export default App;
+
+
